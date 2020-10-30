a17bd0950a3854597bad8137b755fae224e79c21
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-30 21:08:08 +0300

    my commit add about us template and code

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'temps/index.html')
+    return render(request, 'temps/about_us.html')
